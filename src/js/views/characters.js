d02cc6b523164be5f99073c4fb6560fe852356ff
Álvaro M. Villar <144016805+amv87@users.noreply.github.com>
2023-11-22 20:15:52 +0000

    created src/js/views/characters.js component

---
+++ src/js/views/characters.js
@@ -0,0 +1,32 @@
+import React, { useState, useEffect, useContext } from "react";
+import { Link } from "react-router-dom";
+import { Context } from "../store/appContext";
+
+import "../../styles/demo.css";
+
+export const Characters = () => {
+	const { store, actions } = useContext(Context);
+
+	return (
+		<div className="container">
+            <h1 className="text-start text-danger">Characters</h1>
+			{store.character.map((item, index) => {
+				return (
+                    <div key={index} className="card text-start" style={{width: '18rem'}}>
+                        <img src="..." className="card-img-top" alt="..." />
+                        <div className="card-body">
+                            <h5 className="card-title">{item.name}</h5>
+                            <p className="card-text">Gender: {item.gender}</p>
+                            <p className="card-text">Hair Color: {item.hair_color}</p>
+                            <p className="card-text">Eye Color: {item.eye_color}</p>
+                            <Link to="/">
+                                <button className="btn btn-primary">Learn more!</button>
+                            </Link>
+                        </div>
+                    </div>
+				);
+			})}
+			<br />
+		</div>
+	);
+};
