22315fc71ab216aeaf8bf97cc083354f236aeec7
Álvaro M. Villar <144016805+amv87@users.noreply.github.com>
2023-11-24 20:34:07 +0000

    debugging

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -10,14 +10,14 @@ export const Navbar = () => {
 					<span className="navbar-brand mb-0 h1">Star Wars</span>
 				</Link>
 				<div className="ml-auto">
-					<div class="btn-group">
-						<button type="button" class="btn btn-primary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+					<div className="btn-group">
+						<button type="button" className="btn btn-primary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
 							Favorites
 						</button>
-						<ul class="dropdown-menu">
-							<li><a class="dropdown-item" href="#">Action</a></li>
-							<li><a class="dropdown-item" href="#">Another action</a></li>
-							<li><a class="dropdown-item" href="#">Something else here</a></li>
+						<ul className="dropdown-menu">
+							<li><a className="dropdown-item" href="#">Action</a></li>
+							<li><a className="dropdown-item" href="#">Another action</a></li>
+							<li><a className="dropdown-item" href="#">Something else here</a></li>
 						</ul>
 					</div>
 				</div>
