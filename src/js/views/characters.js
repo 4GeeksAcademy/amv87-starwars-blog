d010c62e2ae45cf5cb0e0244aa80a2fd3d53cf23
Álvaro M. Villar <144016805+amv87@users.noreply.github.com>
2023-11-26 21:08:24 +0000

    changed styles and working API

--- src/js/views/characters.js
+++ src/js/views/characters.js
@@ -11,9 +11,9 @@ export const Characters = () => {
 		<div className="container">
             <h1 className="text-start text-danger">Characters</h1>
             <div className="row horizontal">
-                {store.character.map((item, index) => {
+                {store.character.map((item) => {
                     return (
-                        <div key={index} className="card text-start mx-3 p-0" style={{width: '400px'}}>
+                        <div key={item.url} className="card text-start mx-3 my-3 p-0 carditem" style={{width: '400px'}}>
                             <img src={SmallImage} className="card-img-top" alt="..." />
                             <div className="card-body">
                                 <h5 className="card-title">{item.name}</h5>
