8bea062aab6acc0b9eddbbf27a3cb8539c7dd5f6
Álvaro M. Villar <144016805+amv87@users.noreply.github.com>
2023-12-02 21:58:17 +0000

    cleared out of unnecessary code

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,6 +1,4 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import { Context } from "../store/appContext";
 import { Characters } from "../views/characters";
 import { Planets } from "../views/planets";
 
