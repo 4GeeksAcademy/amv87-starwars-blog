7521c2251a4284aae66c46ee7fec1490513c6567
Álvaro M. Villar <144016805+amv87@users.noreply.github.com>
2023-12-02 21:57:37 +0000

    cleared out of unnecessary code

--- src/js/layout.js
+++ src/js/layout.js
@@ -1,21 +1,13 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ScrollToTop from "./component/scrollToTop";
-
 import { Home } from "./views/home";
-import { Demo } from "./views/demo";
-import { Single } from "./views/single";
 import injectContext from "./store/appContext";
-
 import { Navbar } from "./component/navbar";
-import { Footer } from "./component/footer";
 import { PlanetDetail } from "./views/planet-detail";
 import { CharacterDetail } from "./views/character-detail";
 
-//create your first component
 const Layout = () => {
-	//the basename is used when your project is published in a subdirectory and not in the root of the domain
-	// you can set the basename on the .env file located at the root of this project, E.g: BASENAME=/react-hello-webapp/
 	const basename = process.env.BASENAME || "";
 
 	return (
@@ -27,8 +19,6 @@ const Layout = () => {
 						<Route path="/" element={<Home />} />
 						<Route path="/character-detail/:character" element={<CharacterDetail />} />
 						<Route path="/planet-detail/:planet" element={<PlanetDetail />} />
-						<Route path="/demo" element={<Demo />} />
-						<Route path="/single/:theid" element={<Single />} />
 						<Route path="*" element={<h1>Not found!</h1>} />
 					</Routes>
 				</ScrollToTop>
