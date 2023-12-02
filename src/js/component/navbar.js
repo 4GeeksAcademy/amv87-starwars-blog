dea56b0eb318ead489405ca8317f83a299ab42fd
Álvaro M. Villar <144016805+amv87@users.noreply.github.com>
2023-12-02 21:38:21 +0000

    added two different mappings

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -14,13 +14,23 @@ export const Navbar = () => {
 				<div className="ml-auto">
 					<div className="btn-group">
 						<button type="button" className="btn btn-lg btn-primary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
-							Favorites <span className="rounded bg-secondary mx-2 px-2">{store.favorites.length}</span>
+							Favorites <span className="rounded bg-secondary mx-2 px-2">{store.favorites1.length + store.favorites2.length}</span>
 						</button>
 						<ul className="dropdown-menu dropdown-menu-end">
-							{store.favorites.map((item)=>
+							{store.favorites1.map((item)=>
 								<li className="px-2 d-flex justify-content-between align-items-center">
-									<a className="dropdown-item d-flex justify-content-between" href="#">{item}</a>
-									<a onClick={()=>actions.deleteFavorite(item)} className="btn btn-danger btn-sm" >X</a>
+									<Link to={"/character-detail/" + item.id}>
+									<a className="dropdown-item d-flex justify-content-between" >{item.name}</a>
+									</Link>
+									<a onClick={()=>actions.deleteFavorite1(item.name)} className="btn btn-danger btn-sm" >X</a>
+								</li>
+							)}
+							{store.favorites2.map((item)=>
+								<li className="px-2 d-flex justify-content-between align-items-center">
+									<Link to={"/planet-detail/" + item.id}>
+									<a className="dropdown-item d-flex justify-content-between" >{item.name}</a>
+									</Link>
+									<a onClick={()=>actions.deleteFavorite2(item.name)} className="btn btn-danger btn-sm" >X</a>
 								</li>
 							)}
 						</ul>
