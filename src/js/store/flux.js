72ba92483aeefafd460eb59609e2d9b9737e12c4
Álvaro M. Villar <144016805+amv87@users.noreply.github.com>
2023-12-02 21:39:00 +0000

    adding arrays of objects to make links on favorites work

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -15,25 +15,46 @@ const getState = ({ getStore, getActions, setStore }) => {
 			],
 			character: [],
 			planets: [],
-			favorites: []
+			favorites: [],
+			favorites1: [],
+			favorites2: []
 		},
 		actions: {
 			// Use getActions to call a function within a fuction
 			exampleFunction: () => {
 				getActions().changeColor(0, "green");
 			},
-			addFavorite: (favoriteName) => {
+			addFavorite1: (favoriteName, index) => {
 				const store = getStore();
 				
-				if (store.favorites.includes(favoriteName)) {
+				if (store.favorites1.includes(favoriteName) || store.favorites.includes(favoriteName)) {
 					setStore({ favorites: store.favorites.filter((yet)=> yet !== favoriteName) });
+					setStore({ favorites1: store.favorites1.filter((yet)=> yet.name !== favoriteName) });
 				} else {
 					setStore({ favorites: [...store.favorites , favoriteName] });
+					setStore({ favorites1: [...store.favorites1 , {name: favoriteName, id: index}] });
 				}
 			},
-			deleteFavorite: (favoriteName) => {
+			addFavorite2: (favoriteName, index) => {
+				const store = getStore();
+				
+				if (store.favorites2.includes(favoriteName) || store.favorites.includes(favoriteName)) {
+					setStore({ favorites: store.favorites.filter((yet)=> yet !== favoriteName) });
+					setStore({ favorites2: store.favorites2.filter((yet)=> yet.name !== favoriteName) });
+				} else {
+					setStore({ favorites: [...store.favorites , favoriteName] });
+					setStore({ favorites2: [...store.favorites2 , {name: favoriteName, id: index}] });
+				}
+			},
+			deleteFavorite1: (favoriteName) => {
 				const store = getStore();
 				setStore({ favorites: store.favorites.filter((yet)=> yet !== favoriteName) });
+				setStore({ favorites1: store.favorites1.filter((yet)=> yet.name !== favoriteName) });
+			},
+			deleteFavorite2: (favoriteName) => {
+				const store = getStore();
+				setStore({ favorites: store.favorites.filter((yet)=> yet !== favoriteName) });
+				setStore({ favorites2: store.favorites2.filter((yet)=> yet.name !== favoriteName) });
 			},
 			loadSomeData: () => {
 				/**
